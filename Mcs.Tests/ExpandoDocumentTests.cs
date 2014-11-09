d3cb0fc380be202e02e660675314eeb57e115c02
Glenn <glenn.android@gmail.com>
2014-11-09 22:01:42 +0000

    BsonDocument to ExpandoObject helper

---
+++ Mcs.Tests/ExpandoDocumentTests.cs
@@ -0,0 +1,39 @@
+﻿namespace Mcs.Tests
+{
+    using System;
+    using System.Dynamic;
+
+    using Mcs.Helpers;
+    using Mcs.Models;
+
+    using MongoDB.Bson;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ExpandoDocumentTests
+    {
+        [TestCase]
+        public void Foo()
+        {
+            var doc = new BsonDocument
+                      {
+                          { "firstName", "Homer" },
+                          { "birthDate", DateTime.Parse("2000-01-01") },
+                          { "address", new BsonDocument { { "line1", "10 Acacia Avenue" } } }
+                      };
+
+            dynamic d = doc.ToExpando();
+
+            dynamic y = new ExpandoObject();
+
+            y.arse = "hello";
+
+            Console.WriteLine(y.arse);
+
+            string x = d.firstName;
+
+            
+        }
+    }
+}
